1e805949f6a2a5c289ebecda5e3fe6afce40b380
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-15 05:51:22 -0700

    2 commit

--- main.cpp
+++ main.cpp
@@ -8,5 +8,17 @@ int main() {
     do {
         cout << "\nIs it raining? (1-Yes, 0-No): ";
         cin >> raining;
+        if (raining) {
+            cout << "Finding umbrella\n";
+            cout << "Did you find the umbrella? (1-Yes, 0-No): ";
+            cin >> umbrellaFound;
+
+            if (umbrellaFound) {
+                cout << "Bring umbrella.\n";
+            } else {
+                cout << "Can't go without umbrella, checking again...\n";
+                continue; // loop back to check whether it is raining again
+            }
+        }
 
 }
